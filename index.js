7a9a4d8702f9928d79a7a79c366a634f83f958c9
Greg Morgan <gregm9999@gmail.com>
2019-01-19 21:46:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById("nested").querySelector(".target");
+  return document.querySelector("nested .target");
 }
 
 function deepestChild(x) {
