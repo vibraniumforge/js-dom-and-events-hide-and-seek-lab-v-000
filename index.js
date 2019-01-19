549121e77d2436502c28694d1bead67bc9647cac
Greg Morgan <gregm9999@gmail.com>
2019-01-19 21:48:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector("nested .target");
+  return document.querySelector("#nested .target");
 }
 
 function deepestChild() {
