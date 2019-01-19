08563fbba762f55ef90b1b422bb4ee41e92a36ab
Greg Morgan <gregm9999@gmail.com>
2019-01-19 21:57:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nodeChild = node.children[0];
+  while (nodeChild) {
+    node=nodeChild;
+    nodeChild=node.children[0];
+  }
+  return node;
+}
+
+function increaseRankBy(n) {
+  let int=parseInt(n, 10)
+  let rankedLists = document.querySelectorAll(".ranked-list");
+  for (let i=0; i< rankedLists.length; i++) {
+    let children=rankedLists[i].children;
+    for (let j=0; j< children.length; j++) {
+      children[j].innerHTML=parseInt(children[j].innerHTML, 10)+int;
+    }
+  }
+}
